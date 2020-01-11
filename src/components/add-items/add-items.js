dd6678cd5e0cd430b66f481b71d46d7a96566f05
Mac <mac@MacBook-Pro-Mac.local>
2020-01-11 21:27:38 +0200

    refactoring, complete adding tasks in form

--- src/components/add-items/add-items.js
+++ src/components/add-items/add-items.js
@@ -4,18 +4,40 @@ import './add-items.css';
 
 export default class AddItem extends React.Component {
 
+  state = {
+    label: ''
+  };
+
+  onLabelChange = ( event ) => {
+    this.setState( {
+      label: event.target.value
+    } );
+  };
+
+  onSubmit = ( event ) => {
+    event.preventDefault()
+    this.props.onAdd(this.state.label);
+    this.setState({
+      label: ''
+    })
+  };
 
   render() {
-    const { onAdd } = this.props;
 
     return (
-        <div className='item-add-form'>
-          <input type="text" className='form-control'/>
-          <button className='btn btn-outline-secondary'
-                  onClick={ () => onAdd('hello') }>
+        <form className='item-add-form'
+              onSubmit={ this.onSubmit }
+        >
+          <input type="text"
+                 className='form-control'
+                 onChange={ this.onLabelChange }
+                 placeholder='What needs to be done'
+                 value={this.state.label}
+          />
+          <button className='btn btn-outline-secondary'>
             Add new task
           </button>
-        </div>
+        </form>
 
     );
   }
--- src/components/app/app.js
+++ src/components/app/app.js
@@ -14,11 +14,35 @@ export default class App extends React.Component {
 
   state = {
     todoData: [
-      { label: 'drink coffee', important: false, id: 1 },
-      { label: 'make awesome app', important: true, id: 2 },
-      { label: 'have a launch', important: false, id: 3 }
+      this.addItem( 'Drink coffee' ),
+      this.addItem( 'Fall asleep' ),
+      this.addItem( 'Find Project' )
     ]
   };
+
+  addItem( label ) {
+    return {
+      label,
+      important: false,
+      done: false,
+      id: this.maxId++
+    };
+  };
+
+  toggleProperty( arr, id, propName ) {
+    const idx = arr.findIndex( ( el ) => el.id === id );
+
+    const oldItem = arr[ idx ];
+    const newItem = { ...oldItem, [ propName ]: !oldItem[ propName ] };
+
+    return [
+      ...arr.slice( 0, idx ),
+      newItem,
+      ...arr.slice( idx + 1 )
+    ];
+
+
+  }
 
   deleteItem = ( id ) => {
     this.setState( ( { todoData } ) => {
@@ -35,7 +59,7 @@ export default class App extends React.Component {
 
   createItem = ( text ) => {
 
-    const newItem = { label: text, important: false, id: this.maxId++ };
+    const newItem = this.addItem( text );
 
     this.setState( ( { todoData } ) => {
       const newState = [ ...todoData, newItem ];
@@ -45,29 +69,41 @@ export default class App extends React.Component {
     } );
   };
 
-  onToggleImportant = (id) => {
-  console.log(id)
-  }
+  onToggleImportant = ( id ) => {
+    this.setState( ( { todoData } ) => {
+      return {
+        todoData: this.toggleProperty( todoData, id, 'important' )
+      };
+    } );
+  };
 
-  onToggleDone = (id) => {
-console.log(id)
-  }
+  onToggleDone = ( id ) => {
+    this.setState( ( { todoData } ) => {
+      return {
+        todoData: this.toggleProperty( todoData, id, 'done' )
+      };
+    } );
+  };
 
 
   render() {
+    const { todoData } = this.state;
+
+    const doneCount = todoData.filter( el => el.done ).length;
+    const todoCount = todoData.length - doneCount;
 
     return (
         <div className='todo-app'>
-          <AppHeader todo={ 1 } done={ 3 }/>
+          <AppHeader todo={ todoCount } done={ doneCount }/>
           <div className='top-panel d-flex'>
             <SearchPanel/>
             <ItemStatusFilter/>
           </div>
 
           <TodoList todos={ this.state.todoData }
-                    onDeleted={this.deleteItem}
-                    onToggleImportant={this.onToggleImportant}
-                    onToggleDone={this.onToggleDone}
+                    onDeleted={ this.deleteItem }
+                    onToggleImportant={ this.onToggleImportant }
+                    onToggleDone={ this.onToggleDone }
           />
           <AddItem onAdd={ ( text ) => {
             this.createItem( text );
